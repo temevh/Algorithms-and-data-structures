03a69ada4112a19acb77c36a2983460f1cc4267d
temevh <temehama@gmail.com>
2022-10-31 10:14:00 +0200

    Subsets init

---
+++ subsets.py
@@ -0,0 +1,2 @@
+#A program to create subsets with a given int.
+#For example subsets(int 3) -> [1],[2],[3],[1,2],[2,3][1,2,3]
--- testi.py
+++ testi.py
@@ -1,1 +1,8 @@
-#testi
+lista = [4,1,51,7,125,4,1,5,4,4,4,16,159569,15,5124,731,51,125,16,4,1]
+
+listarem = []
+for i in lista:
+    if i not in listarem:
+        listarem.append(i)
+
+print(listarem)
