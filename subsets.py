40161239d405dd0c8e330dc4e44ab0392774c36c
temevh <temehama@gmail.com>
2022-10-31 10:45:42 +0200

    Using double for-loops

--- subsets.py
+++ subsets.py
@@ -1,14 +1,36 @@
 #A program to create subsets with a given int.
-#For example subsets(int 3) -> [1],[2],[3],[1,2],[2,3][1,2,3]
+#For example subsets(int 3) -> [1],[2],[3],[1,2],[2,3][1,2,3] (powerset?)
 
-
+'''
 def subsets(n: int) -> list:
     subs = []
+    aux = []
+    max = 0
     for i in range(1, n+1):
         sub = [i]
         subs.append(sub)
+        max = i
+        for j in range(1, max):
+            aux.append(j)
+        subs.append(aux)
 
     return subs
+'''
+def subsets(n: int) -> list:
+    fullset = []
+    for i in range(1, n+1):
+        fullset.append(i)
+    print(fullset)
+    subs = []
+    for i in range(2**len(fullset)):
+        subset = []
+        for k in range(len(fullset)):
+            if i & 1<<k:
+                subset.append(fullset[k])
+        subs.append(subset)
+    subs.pop(0)
+    return subs
+
 
 if __name__ == "__main__":
     print(subsets(3))   # [[1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
