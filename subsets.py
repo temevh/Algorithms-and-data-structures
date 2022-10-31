7d9298524df849802dab457050cb8ed1fb63e3fb
temevh <temehama@gmail.com>
2022-10-31 11:00:30 +0200

    Working

--- subsets.py
+++ subsets.py
@@ -1,35 +1,22 @@
 #A program to create subsets with a given int.
 #For example subsets(int 3) -> [1],[2],[3],[1,2],[2,3][1,2,3] (powerset?)
 
-'''
-def subsets(n: int) -> list:
-    subs = []
-    aux = []
-    max = 0
-    for i in range(1, n+1):
-        sub = [i]
-        subs.append(sub)
-        max = i
-        for j in range(1, max):
-            aux.append(j)
-        subs.append(aux)
-
-    return subs
-'''
 def subsets(n: int) -> list:
     fullset = []
+    subs = []
     for i in range(1, n+1):
         fullset.append(i)
-    print(fullset)
-    subs = []
-    for i in range(2**len(fullset)):
+    #print(fullset)
+    for j in range(2**len(fullset)):
         subset = []
         for k in range(len(fullset)):
-            if i & 1<<k:
+            if j & 1*2**k:
                 subset.append(fullset[k])
         subs.append(subset)
     subs.pop(0)
     return subs
+
+
 
 
 if __name__ == "__main__":
@@ -39,7 +26,7 @@ if __name__ == "__main__":
                         #  [4], [1, 4], [2, 4], [1, 2, 4], [3, 4], [1, 3, 4],
                         #  [2, 3, 4], [1, 2, 3, 4]]
 
-    #S = subsets(10)
-    #print(S[95])    # [6, 7]
-    #print(S[254])   # [1, 2, 3, 4, 5, 6, 7, 8]
-    #print(S[826])   # [1, 2, 4, 5, 6, 9, 10]
+    S = subsets(10)
+    print(S[95])    # [6, 7]
+    print(S[254])   # [1, 2, 3, 4, 5, 6, 7, 8]
+    print(S[826])   # [1, 2, 4, 5, 6, 9, 10]
