69129ef94c8763f1762607fd396082003a8f3c24
temevh <temehama@gmail.com>
2022-10-31 10:17:48 +0200

    First test

--- subsets.py
+++ subsets.py
@@ -1,2 +1,23 @@
 #A program to create subsets with a given int.
 #For example subsets(int 3) -> [1],[2],[3],[1,2],[2,3][1,2,3]
+
+
+def subsets(n: int) -> list:
+    subs = []
+    for i in range(n+1):
+        sub = [i]
+        subs.append(sub)
+
+    return subs
+
+if __name__ == "__main__":
+    print(subsets(3))   # [[1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+    #print(subsets(4))   # [[1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3],
+                        #  [4], [1, 4], [2, 4], [1, 2, 4], [3, 4], [1, 3, 4],
+                        #  [2, 3, 4], [1, 2, 3, 4]]
+
+    #S = subsets(10)
+    #print(S[95])    # [6, 7]
+    #print(S[254])   # [1, 2, 3, 4, 5, 6, 7, 8]
+    #print(S[826])   # [1, 2, 4, 5, 6, 9, 10]
