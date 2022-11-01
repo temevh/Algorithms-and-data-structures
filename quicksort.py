c1d15c8dd1d6eb5efff42c162ff2603f036f9f0f
temevh <temehama@gmail.com>
2022-11-01 10:25:39 +0200

    Working quicksort, no need for pivot function

--- quicksort.py
+++ quicksort.py
@@ -2,26 +2,36 @@
 #A: list of integers, i: start index of the partition, j: end index of selected partition
 
 def qsort(A, i, j):
-    pivot = findpivot(A,i,j)
-    A[pivot], A[j] = A[j], A[pivot]
-    k = partition(A, i, j-1, A[j])
-    A[k], A[j] = A[j], A[k]
-    if ((k-i) > 1):
-        qsort(A, i, k-1)
-    if ((j-k) > 1):
-        qsort(A, k+1, j)
+    #pivot = findpivot(i,j)
+    #A[pivot], A[j] = A[j], A[pivot]
+    #print(A)
+    if i >= j:
+        return
+    k = partition(A, i, j, A[i])
+    #A[k], A[j] = A[j], A[k]
+    #if ((k-i) > 1):
+    qsort(A, i, k-1)
+    #if ((j-k) > 1):
+    qsort(A, k+1, j)
 
-def partition(A, left, right, pivot):
-    while (left <= right):
-        while(A[left] != pivot):
+
+def partition(A, i, right, pivot):
+    left = i + 1
+    while True:
+        while left <= right and A[right] >= pivot:
+            right -=1
+        while left <= right and A[left] <= pivot:
             left += 1
-        while((right >= left) and (A[right] >= pivot)):
-            right -= 1
-        if right > left:
+        if left <= right:
             A[left], A[right] = A[right], A[left]
-    return left
+        else:
+            break
+    A[i], A[right] = A[right], A[i]
 
-def findpivot(A, i, j):
+    return right
+
+
+def findpivot(i, j):
     return(int((i+j)/2))
 
 if __name__ == "__main__":
