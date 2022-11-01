1d3954c2596343184edbdcbfd2430193c0a0af49
temevh <temehama@gmail.com>
2022-11-01 09:36:24 +0200

    Fixed recursion error

--- quicksort.py
+++ quicksort.py
@@ -7,10 +7,9 @@ def qsort(A, i, j):
     k = partition(A, i, j-1, A[j])
     A[k], A[j] = A[j], A[k]
     if ((k-i) > 1):
-        qsort(A, i, k-i)
+        qsort(A, i, k-1)
     if ((j-k) > 1):
         qsort(A, k+1, j)
-
 
 def partition(A, left, right, pivot):
     while (left <= right):
@@ -26,7 +25,6 @@ def partition(A, left, right, pivot):
 def findpivot(A, i, j):
     return(int((i+j)/2))
 
-  
 if __name__ == "__main__":
     A = [9, 7, 1, 8, 5, 3, 6, 2, 4]
     print(A)    # [9, 7, 1, 8, 5, 3, 6, 2, 4]
