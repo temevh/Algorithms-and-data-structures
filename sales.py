676dcbcdaa23c77e50d14858f2eb516d7ab1942a
temevh <temehama@gmail.com>
2022-11-02 12:30:29 +0200

    Working, passing functionality and complexity

--- sales.py
+++ sales.py
@@ -1,16 +1,19 @@
 def sales(cars, customers) -> int:
-    possible = 0
     customers.sort()
     cars.sort()
-    print(customers)
-    print(cars)
-    for i in range(len(customers)):
-        customer = customers[i]
-        car = cars[i]
+    customerIndex = 0
+    carIndex = 0
+    while customerIndex < len(customers):
+        customer = customers[customerIndex]
+        if carIndex >= len(cars):
+            break
+        car = cars[carIndex]
         if car <= customer:
-            i += 1
-
-    return i
+            customerIndex += 1
+            carIndex += 1
+        else:
+            customerIndex += 1
+    return carIndex
 
 
 if __name__ == "__main__":
@@ -18,3 +21,4 @@ if __name__ == "__main__":
     print(sales([13, 7, 2, 3, 12, 4, 19], [3, 25, 16, 14]))         # 4
     print(sales([24, 6, 20, 21, 12, 5], [25, 1, 24, 15]))           # 3
     print(sales([14, 9, 10, 15, 18, 20], [24, 17, 9, 22, 12, 4]))   # 5
+    print(sales([0, 1, 2], [10, 10, 10, 10, 10, 10]))
