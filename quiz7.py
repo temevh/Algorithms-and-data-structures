f685e02a6977c3679de0468faf7812632d3c31c6
temevh <temehama@gmail.com>
2022-11-03 09:38:04 +0200

    Working quiz7 + txt of output

--- quiz7.py
+++ quiz7.py
@@ -1,15 +1,18 @@
-def permutation(k):
-    numbers = []
-    n = 5
+n = 5
+included = [0, 0, 0, 0, 0]
+numbers = [1, 2, 3, 4, 5]
+
+
+def permutations(k):
     if k == n:
         print(numbers)
     else:
-        for i in range(1, n):
-            if i not in numbers:
-                numbers[k] = i
-                included = True
-                permutation(k+1)
-                included = False
+        for i in range(n):
+            if not included[i]:
+                included[i] = True
+                numbers[k] = i+1
+                permutations(k+1)
+                included[i] = False
 
 
-permutation(1)
+permutations(0)
