db1ccb69a75bb02f660f8c1434268ccf328cc44d
temevh <temehama@gmail.com>
2022-11-03 09:40:21 +0200

    added hasher

--- hash_1.py
+++ hash_1.py
@@ -1,2 +1,12 @@
 # BM40A1500 Algorithms and data structures
 # Practical assignment 1, Implementing a hash table
+class HashTable:
+    def __init__(self) -> None:
+        self.SIZE = 100
+        self.arr = [None for i in range(self.SIZE)]
+
+    def hasher(self, key):
+        v = 0
+        for c in str(key):
+            h += ord(c)
+        return h % self.SIZE
