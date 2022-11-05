7fce6a30c0bb2e95e4ffd604f2626e8d81c513ad
temevh <temehama@gmail.com>
2022-11-05 10:13:31 +0200

    Adde commenting

--- Assingment/hash_1.py
+++ Assingment/hash_1.py
@@ -5,7 +5,7 @@
 # Average Case O(1)
 # Worst case O(n) if all keys have the same hash, essentially the program goes through a linked list, and the complexity for list look-ups is O(n)
 # Duplicates not allowed
-# Using % self.size as the hasher, with large files most of the linked lists are left empty
+
 
 import time
 
@@ -17,46 +17,49 @@ class HashTable:
         self.arr = [[] for i in range(self.SIZE)]
 
     def hasher(self, key):  # Calculate hash using string folding
-        key = str(key)
-        sum = 0
-        mul = 1
+        key = str(key)  # Make sure the given key is a string
+        sum = 0  # Initializa sum to zero
+        mul = 1  # Initialize mul to 1
         for i in range(len(str(key))):
-            if (i % 4 == 0):
+            if (i % 4 == 0):  # Process the key 4 letters at a time
                 mul = 1
             else:
                 mul = mul * 256
+            # Sum the ascii values of the key's characters, after multiplying with mul(tiplier)
             sum += ord(key[i]) * mul
+        # End result is converted to the range 0 to M-1 using the hash table size and modulo operators
         return sum % self.SIZE
 
     def adder(self, key):
-        h = self.hasher(key)
-        key = str(key)
-        # print("hastattu", h)
-        found = False
+        h = self.hasher(key)  # Calculate hash
+        key = str(key)  # Make sure key is string
+        found = False  # Set found to false, found is used to check if key already exists, thus not allowing duplicates
+        # By using enumerate, we can implement a counter in the loop (index)
         for index, element in enumerate(self.arr[h]):
+            # If the length of the element is 2 and the current element is key
             if len(element) == 2 and element[0] == key:
                 self.arr[h][index] = key
-                found = True
-                break
-        if not found:
+                found = True  # Set found to true
+                break  # Do not add the key to the array
+        if not found:  # Else if the loop does not encounter the key
+            # Add the key to the given list at the array
             self.arr[h].append(key)
 
     def getter(self, key):
         key = str(key)
-        spot = ""
+        spot = ""  # A placeholder variable for the potential info that is to be returned if the given key is found
         h = self.hasher(key)  # Calculate the hash
-        # loop through the linked list at the given index(hash)
-        # for element in self.arr[h]:
         i = 0
+        # loop through the linked list at the given index(hash)
         for element in self.arr[h]:
             i += 1
-            if element == key:
+            if element == key:  # If the current loop element matches the key to be searched for
                 spot = "Key found\nKey hash: " + \
                     str(h) + "\nKey is the " + str(i) + \
                     " nth element in list " + str(h)
-                return spot
+                return spot  # Add information to the spot variable, return it
         else:
-            print("key not found")
+            print("key not found")  # If key not found, return "key not found"
 
     def delete(self, key):
         h = self.hasher(key)  # Calculate the hash
@@ -77,6 +80,7 @@ class HashTable:
             # Pass the word from the file to the hash table, removing the newline(\n)
             self.adder(line.strip())
 
+    # A test function to write the hash table to file, to make sure everything gets added
     def writeToFile(self):
         file = open("hashResult.txt", "w", encoding="utf-8")
         for element in self.arr:
@@ -90,6 +94,7 @@ t.adder("teesti")
 t.adder("auton moottori")
 t.adder("tietokone")
 t.adder(11115111111)
+t.adder("te")
 t.writeToFile()
 print(t.getter(141))
 print(t.getter("teesti"))
