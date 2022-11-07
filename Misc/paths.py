e502657350e80c698e4b065fa599a986491c34c9
temevh <temehama@gmail.com>
2022-11-07 13:03:59 +0200

    working hash 1 2 and 3

--- Misc/paths.py
+++ Misc/paths.py
@@ -1,22 +1,14 @@
-def paths(n, a, b):
-    possible = 0
-    while True:
-        if n >= a:
-            n = n-a
-            possible += 1
-        elif n >= b:
-            n = n-b
-            possible += 1
-        if n == 0:
-            break
-        print("n: ", n)
+import math
 
-    return possible
+
+def paths(n, a, b):
+    if (a + b) > n:
+        return 0
 
 
 if __name__ == "__main__":
     print(paths(4, 1, 2))  # 5
-    # print(paths(8, 2, 3))  # 4
-    # print(paths(11, 6, 7))  # 0
-    # print(paths(30, 3, 5))  # 58
-    # print(paths(100, 4, 5))  # 1167937
+    print(paths(8, 2, 3))  # 4
+    print(paths(11, 6, 7))  # 0
+    print(paths(30, 3, 5))  # 58
+    print(paths(100, 4, 5))  # 1167937
