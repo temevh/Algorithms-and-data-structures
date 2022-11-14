89e13fc7770ac539de578f02e20b88cfcf6a5521
temevh <temehama@gmail.com>
2022-11-14 21:15:19 +0200

    started to implement dfs

--- Misc/graph.py
+++ Misc/graph.py
@@ -1,34 +1,22 @@
+# https://www.askpython.com/python/examples/depth-first-search-in-a-graph
+# https://www.codingninjas.com/codestudio/library/implementation-of-dfs-using-adjacency-matrix
 class Graph():
     def __init__(self, grid):
-        self.matrix = matrix
+        self.matrix = grid
 
     def df_print(self, start):
-        stack = [[0, 0]]
+        # adjency matrix -> adjency list
 
-        while len(stack) != 0:
-            value = stack.pop(0)
-            self.matrix[value[0]][value[1]] = '1'
+        #toReturn = self.df_help(graph, start, path=[])
+        # print(toReturn)
 
-            if (value[0] < len(self.matrix)-1):
-                down = [value[0]+1, value[1]]
-                matrixValue = matrix[down[0]][down[1]]
-                if (matrixValue != '1'):
-                    stack.append(down)
+    def df_help(self, graph, vertex, path=[]):
+        path += [vertex]
+        for n in graph[vertex]:
+            if n not in path:
+                path = self.df_help(graph, n, path)
 
-            if (value[1] < len(matrix[0]) - 1 and value[0] < len(matrix) - 1):
-                diagonal = [value[0] + 1, value[1] + 1]
-                matrixValue = self.matrix[diagonal[0]][diagonal[1]]
-                if matrixValue != '1':
-                    stack.append(diagonal)
-
-            if (value[1] < len(matrix[0]) - 1):
-                right = [value[0], value[1] + 1]
-                matrixValue = self.matrix[right[0]][right[1]]
-                if matrixValue != '1':
-                    stack.append(right)
-
-        print(self.matrix)
-        print(stack)
+        return path
 
     def bf_print(self, start):
         pass
@@ -48,7 +36,6 @@ if __name__ == "__main__":
         [0, 0, 0, 0, 0, 1],  # 4
         [0, 6, 0, 0, 0, 0]  # 5
     ]
-
     graph = Graph(matrix)
 
     graph.df_print(0)           # 0 2 1 3 5 4
