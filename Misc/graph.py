b7eddf00407c18cca162ab2114664913643ec913
temevh <temehama@gmail.com>
2022-11-14 10:18:15 +0200

    first try depth-first traversal

--- Misc/graph.py
+++ Misc/graph.py
@@ -1,7 +1,34 @@
-class Graph:
+class Graph():
+    def __init__(self, grid):
+        self.matrix = matrix
 
     def df_print(self, start):
-        pass
+        stack = [[0, 0]]
+
+        while len(stack) != 0:
+            value = stack.pop(0)
+            self.matrix[value[0]][value[1]] = '1'
+
+            if (value[0] < len(self.matrix)-1):
+                down = [value[0]+1, value[1]]
+                matrixValue = matrix[down[0]][down[1]]
+                if (matrixValue != '1'):
+                    stack.append(down)
+
+            if (value[1] < len(matrix[0]) - 1 and value[0] < len(matrix) - 1):
+                diagonal = [value[0] + 1, value[1] + 1]
+                matrixValue = self.matrix[diagonal[0]][diagonal[1]]
+                if matrixValue != '1':
+                    stack.append(diagonal)
+
+            if (value[1] < len(matrix[0]) - 1):
+                right = [value[0], value[1] + 1]
+                matrixValue = self.matrix[right[0]][right[1]]
+                if matrixValue != '1':
+                    stack.append(right)
+
+        print(self.matrix)
+        print(stack)
 
     def bf_print(self, start):
         pass
@@ -25,6 +52,6 @@ if __name__ == "__main__":
     graph = Graph(matrix)
 
     graph.df_print(0)           # 0 2 1 3 5 4
-    graph.bf_print(0)           # 0 2 4 1 3 5
-    print(graph.weight(0, 2))   # 7
-    print(graph.weight(3, 4))   # -1
+    # graph.bf_print(0)           # 0 2 4 1 3 5
+    # print(graph.weight(0, 2))   # 7
+    # print(graph.weight(3, 4))   # -1
