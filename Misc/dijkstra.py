4f81aa6521bd266e04eef7ccd44648b8cfc935a0
temevh <temehama@gmail.com>
2022-11-22 09:41:37 +0200

    improved dijkstra

--- Misc/dijkstra.py
+++ Misc/dijkstra.py
@@ -3,30 +3,45 @@ from graph import Graph
 
 
 def dijkstragraph(G, s):
-    G.D = [10000000000000] * graph.vertex_count
+    G.alreadySeen = []
+    G.D = [10000000000000] * G.vertex_count
     G.D[s] = 0
-    for i in range(graph.vertex_count):
-        v = minvertex(G)
-        graph.visited[v] = True
+    toReturn = [[0 for i in range(G.vertex_count)]
+                for j in range(G.vertex_count)]
+    for j in range(G.vertex_count):
+        G.alreadySeen.append([j, None])
+    for i in range(G.vertex_count):
+        G.lastNodes.append([0, 0])
+    for i in range(G.vertex_count):
+        v = minVertex(G, G.D)
+        G.alreadySeen[v][1] = True
         if G.D[v] == 10000000000000:
-            return
-        nList = graph.neighbors(v)
+            continue
+        nList = G.neighbors(v)
         for j in range(len(nList)):
             w = nList[j]
-            if (G.D[w]) > (G.D[v]) + G.weight(v, w):
+            if (G.D[w]) > ((G.D[v]) + G.weight(v, w)):
                 G.D[w] = G.D[v] + G.weight(v, w)
+                G.lastNodes[w] = [v, w]
+    for i in range(len(G.lastNodes)):
+        x = G.lastNodes[i][0]
+        y = G.lastNodes[i][1]
+        if G.weight(x, y) == -1:
+            continue
+        toReturn[x][y] = G.weight(x, y)
+    G.matrix = toReturn
     return G
 
 
-def minvertex(G):
+def minVertex(G, distances):
     v = 0
-    for i in range(graph.vertex_count):
-        if graph.visited[i] != True:
+    for i in range(G.vertex_count):
+        if G.alreadySeen[i][1] != True:
             v = i
             break
-        for i in range(graph.vertex_count):
-            if graph.visited[i] != True and G.D[i] < G.D[v]:
-                v = i
+    for i in range(G.vertex_count):
+        if G.alreadySeen[i][1] != True and distances[i] < distances[v]:
+            v = i
     return v
 
 
@@ -45,9 +60,20 @@ if __name__ == "__main__":
         [0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
     ]
 
-    graph = Graph(matrix)
+    matrix2 = [
+        [0, 3, 5, 6, 0, 0, 0, 5],
+        [3, 0, 8, 0, 7, 8, 0, 1],
+        [5, 8, 0, 5, 7, 0, 6, 0],
+        [6, 0, 5, 0, 0, 6, 0, 0],
+        [0, 7, 7, 0, 0, 0, 8, 2],
+        [0, 8, 0, 6, 0, 0, 5, 0],
+        [0, 0, 6, 0, 8, 5, 0, 0],
+        [5, 1, 0, 0, 2, 0, 0, 0]
+    ]
+
+    graph = Graph(matrix2)
     new_graph = dijkstragraph(graph, 0)
     new_graph.df_print(0)           # 0 1 2 3 4 5 6 7 9 8
-    # new_graph.bf_print(0)           # 0 1 2 3 4 5 6 7 8 9
-    # print(new_graph.weight(3, 6))   # 4
-    # print(new_graph.weight(5, 8))   # -1
+    new_graph.bf_print(0)           # 0 1 2 3 4 5 6 7 8 9
+    print(new_graph.weight(3, 6))   # 4
+    print(new_graph.weight(5, 8))   # -1
