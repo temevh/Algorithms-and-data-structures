4541b9ab46e48626a4326e3e91c7ddf3ca0d3a59
temevh <temehama@gmail.com>
2022-11-30 15:02:01 +0200

    some comments

--- Misc/binpack.py
+++ Misc/binpack.py
@@ -1,11 +1,13 @@
-def binpack(items, S):
-    bins = []
-    bin = []
-    size = S
+def binpack(items, B):
+    bins, bin = [], []
+    size = B
 
-    ctrSize = len(items) ** 10
-    counter = ctrSize
-    items.sort()
+    # Useampi kori -> käy koreja läpi kunnes esine mahtuu
+    # Jos ei mahdu mihinkää, luo uusi
+    ctr = len(items)
+    counter = ctr
+    items.sort(reverse=True)
+    print(items)
     while items != []:
         for i in items:
             counter += 1
@@ -13,21 +15,23 @@ def binpack(items, S):
                 size -= i
                 bin.append(i)
                 items.remove(i)
-                counter = ctrSize
+                counter = ctr
+            elif i > size:  # Jos liian iso, lisää "seuraavaan" koriin
+                pass
             if counter == 0:
                 break
-    
+
         bins.append(bin)
         bin = []
-        size = S
+        size = B
 
     return bins
 
-    
- 
+
 if __name__ == "__main__":
 
     items = [9, 3, 3, 6, 10, 4, 6, 8, 6, 3]
+    #items = [9, 3, 3, 4, 6, 3, 10, 6, 8, 6]
     B = 10
 
     bins = binpack(items, B)
@@ -43,3 +47,31 @@ if __name__ == "__main__":
 #   bin 5: [6]
 #   bin 6: [8]
 #   bin 7: [6]
+
+
+'''
+def binpack(items, S):
+    bins, bin = [], []
+    size = S
+
+    ctrSize = len(items) ** 0.7
+    counter = ctrSize
+    items.sort(reverse=True)
+    print(items)
+    while items != []:
+        for i in items:
+            counter += 1
+            if i <= size:
+                size -= i
+                bin.append(i)
+                items.remove(i)
+                counter = ctrSize
+            if counter == 0:
+                break
+
+        bins.append(bin)
+        bin = []
+        size = S
+
+    return bins
+'''
