8986d559d55f9cad815b52ce948532373205357a
temevh <temehama@gmail.com>
2022-12-02 10:58:56 +0200

    commenting

--- Assingment/HL2.py
+++ Assingment/HL2.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Node:
     def __init__(self, data, next=None):
         self.data = data
@@ -19,9 +22,10 @@ class HashTable:
         hSum = 0  # Initializa sum to zero
         mul = 1  # Initialize mul to 1
         if type(key) == int:
-            for i in range(key):
-                hSum = i * mul * 256
-            return hSum % self.tableSize
+            # for i in range(key):
+            #    hSum = i * mul * 12
+            print((key * 111) % self.tableSize)
+            return (key * 111) % self.tableSize
         else:
             for i in range(len(str(key))):
                 if (i % 4 == 0):  # Process the key 4 letters at a time
@@ -34,28 +38,28 @@ class HashTable:
             return hSum % self.tableSize
 
     def insert(self, key):
-        h = self.hasher(key)
-        addTo = self.lists[h]
-        node = Node(key)
-        if addTo.head is None:
-            addTo.head = node
-            self.printTable()
+        h = self.hasher(key)  # Calculate hash
+        addTo = self.lists[h]  # Choose which list the key should be added to
+        node = Node(key)  # Give the key as data for node
+        if addTo.head is None:  # If the head of the linked list is empty
+            addTo.head = node  # Make the current key/node head
+            # self.printTable()
             return
-        last = addTo.head
-        while last.next:
-            last = last.next
-        last.next = node
-        self.printTable()
+        last = addTo.head  # ITerate through the linked list
+        while last.next:  # While iterator has a next node
+            last = last.next  # Go to next
+        last.next = node  # When the node without next is reached the node is set to that
+        # self.printTable()
 
     def printTable(self):
         for i in range(self.tableSize):
-            x = self.lists[i]
-            itr = x.head
-            strList = []
-            while itr:
-                strList.append(itr.data)
-                itr = itr.next
-            print(i, strList)
+            x = self.lists[i]  # x is the current list to print
+            itr = x.head  # Set iterator to the head of the current list
+            strList = []  # Initiate an empty array for the list elements
+            while itr:  # While list elements exist
+                strList.append(itr.data)  # append list element to strList
+                itr = itr.next  # Move to next element
+            print(i, strList)  # Print the current index and the element
         print()
 
     def find(self, key):
@@ -106,20 +110,25 @@ class HashTable:
 
 ht = HashTable(3)
 
+st = time.time()
 ht.insert(12)
 ht.insert("hashtable")
 ht.insert(1234)
-ht.insert(4328989)
 ht.insert("BM40A1500")
 ht.insert(-12456)
 ht.insert("aaaabbbbcccc")
+ht.insert(151125125125)
+et = time.time()
+# ht.printTable()
 
-ht.search(-12456)
-ht.search("hashtable")
-ht.search(1235)
 
-ht.delete("BM40A1500")
-ht.delete(1234)
-ht.delete("aaaabbbbcccc")
+# ht.search(-12456)
+# ht.search("hashtable")
+# ht.search(1235)
 
-ht.printTable()
+# ht.delete("BM40A1500")
+# ht.delete(1234)
+# ht.delete("aaaabbbbcccc")
+
+# ht.printTable()
+print("TIME TO ADD", (et-st)/6)
